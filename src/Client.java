6e31aa610ce9dd157f4e046a8ec1b02be1ede242
Murel02 <olimurel@gmail.com>
2024-09-12 13:16:09 +0200

    g

--- src/Client.java
+++ src/Client.java
@@ -3,37 +3,41 @@ import java.net.Socket;
 import java.util.Scanner;
 
 public class Client {
-    public static void main(String[] args) {
-        String hostname = "localhost";
+    //public static void main(String[] args) {
+        String hostname = "192.168.0.101";
         int port = 5000;
         String message;
 
-        while (true) {
-            try (Socket socket = new Socket(hostname, port)) {
+        //for (int i = 0; i<500; i++) {
+    Client(int i) {
+        try (Socket socket = new Socket(hostname, port)) {
 
-                System.out.println("Client connected to server");
+            System.out.println("Client connected to server");
 
-                OutputStream output = socket.getOutputStream();
-                PrintWriter writer = new PrintWriter(output, true);
+            OutputStream output = socket.getOutputStream();
+            PrintWriter writer = new PrintWriter(output, true);
 
-                InputStream input = socket.getInputStream();
-                BufferedReader reader = new BufferedReader(new InputStreamReader(input));
+            InputStream input = socket.getInputStream();
+            BufferedReader reader = new BufferedReader(new InputStreamReader(input));
 
-                Scanner messageScanner = new Scanner(System.in);
-                message = messageScanner.nextLine();
+            // Scanner messageScanner = new Scanner(System.in);
+            //message = messageScanner.nextLine();
 
-                writer.println(message);
 
-                String response = reader.readLine();
-                System.out.println("Server response: " + response);
 
-            } catch (FileNotFoundException e) {
-                System.out.println("Error: The file was not found: " + e.getMessage());
-            } catch (IOException e) {
-                System.out.println("IO error: " + e.getMessage());
-                e.printStackTrace(); // for debugging purposes
+            writer.println("Hej" + i);
 
-            }
+            String response = reader.readLine();
+            System.out.println("Server response: " + response);
+
+        } catch (FileNotFoundException e) {
+            System.out.println("Error: The file was not found: " + e.getMessage());
+        } catch (IOException e) {
+            System.out.println("IO error: " + e.getMessage());
+            e.printStackTrace(); // for debugging purposes
+
         }
+        //}
     }
+    //}
 }
---
+++ src/ClientMaker.java
@@ -0,0 +1,8 @@
+public class ClientMaker {
+
+    public static void main(String[] args) {
+        for (int i = 0; i < 50; i++) {
+            Client client = new Client(i);
+        }
+    }
+}
